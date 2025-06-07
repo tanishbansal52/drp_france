9af2c124b9eb2d70470cf7c09fd4e2207fe8e790
Sukruthi <sukruthisantosh@gmail.com>
2025-06-07 22:02:09 +0100

    fixing frontend

--- src/teacher/ShowAllQuestions.jsx
+++ src/teacher/ShowAllQuestions.jsx
@@ -19,7 +19,7 @@ function ShowAllQuestions() {
       setLoading(true)
       setError(null)
 
-      const apiEndpoint = `https://drp-belgium.onrender.com/api/questions-data/${quizId}/`
+      const apiEndpoint = `https://drp-belgium.onrender.com/api/questions`
       const res = await fetch(apiEndpoint)
       if (!res.ok) throw new Error(`HTTP ${res.status}`)
       const data = await res.json()
