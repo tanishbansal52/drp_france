beac46e757e233a1cea205cd29dcf64d8376fa39
TanishB567 <bansaltanish05@gmail.com>
2025-06-16 16:08:58 +0100

    question1 fixed

--- src/student/robotHardCoded/RobotQuestion1.jsx
+++ src/student/robotHardCoded/RobotQuestion1.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import './RobotQuestion1.css';  // add this
 
 function RobotQuestion1() {
   return (
-    <div>
+    <div style={{ fontSize: '1.3rem' }}>
       <h2>Recharge Your Robot!</h2>
       <p>Solve the following arithmetic questions and simplify your answers:</p>
       <ol type="A">
-        <li>1/2 + 1/2</li>
-        <li>1/4 ÷ 1/2</li>
-        <li>3/10 − 1/10</li>
+        <li>
+          <span className="fraction">
+            <span className="num">1</span>
+            <span className="den">2</span>
+          </span>
+          {' '}+{' '}
+          <span className="fraction">
+            <span className="num">1</span>
+            <span className="den">2</span>
+          </span>
+        </li>
+        <li>
+          <span className="fraction">
+            <span className="num">1</span>
+            <span className="den">4</span>
+          </span>
+          {' '}÷{' '}
+          <span className="fraction">
+            <span className="num">1</span>
+            <span className="den">2</span>
+          </span>
+        </li>
+        <li>
+          <span className="fraction">
+            <span className="num">3</span>
+            <span className="den">10</span>
+          </span>
+          {' '}−{' '}
+          <span className="fraction">
+            <span className="num">1</span>
+            <span className="den">10</span>
+          </span>
+        </li>
       </ol>
       <p>The answer is the denominators in order.</p>
     </div>
